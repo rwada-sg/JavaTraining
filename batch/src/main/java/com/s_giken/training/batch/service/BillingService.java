70ab6ac4fb839ea46fc5b900c7b82c6635f6f267
Wada Ryuno <wada-ryuno@s-giken.com>
2026-06-10 08:45:39 +0000

    課題5の修正

--- batch/src/main/java/com/s_giken/training/batch/service/BillingService.java
+++ batch/src/main/java/com/s_giken/training/batch/service/BillingService.java
@@ -1,6 +1,7 @@
 package com.s_giken.training.batch.service;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,41 +25,44 @@ public class BillingService implements IBillingService {
     @Transactional
     public void processBilling(LocalDate targetMonth, LocalDate lastDay) {
 
-        logger.info("{}分の請求情報を確認しています。\n", targetMonth);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年MM月");
+        String formattedTargetMonth = targetMonth.format(formatter);
+
+        logger.info("{}分の請求情報を確認しています。\n", formattedTargetMonth);
 
         Integer commitStatus = billingRepository.checkBillingStatus(targetMonth);
         if (commitStatus != 0) {
-            logger.info("{}分の請求情報は確定済みです。", targetMonth);
+            logger.info("{}分の請求情報は確定済みです。", formattedTargetMonth);
         } else {
             String phase = null;
             try {
-                logger.info("{}分の請求明細データを削除しています。", targetMonth);
+                logger.info("{}分の請求明細データを削除しています。", formattedTargetMonth);
                 phase = "請求明細データ情報の削除";
                 int cntDetailData = billingRepository.deleteBillingDetailData(targetMonth);
                 logger.info("{}件削除しました。", cntDetailData);
 
-                logger.info("{}分の請求データ情報を削除しています。", targetMonth);
+                logger.info("{}分の請求データ情報を削除しています。", formattedTargetMonth);
                 phase = "請求データ情報の削除";
                 int cntData = billingRepository.deleteBillingData(targetMonth);
                 logger.info("{}件削除しました。", cntData);
 
-                logger.info("{}分の請求ステータス情報を削除しています。", targetMonth);
+                logger.info("{}分の請求ステータス情報を削除しています。", formattedTargetMonth);
                 phase = "請求ステータス情報の削除";
                 int cntStatus = billingRepository.deleteBillingStatus(targetMonth);
                 logger.info("{}件削除しました。", cntStatus);
-                logger.info("データベースから{}分の未確定請求情報を削除しました。\n", targetMonth);
+                logger.info("データベースから{}分の未確定請求情報を削除しました。\n", formattedTargetMonth);
 
-                logger.info("{}分の請求ステータス情報を追加しています。", targetMonth);
+                logger.info("{}分の請求ステータス情報を追加しています。", formattedTargetMonth);
                 phase = "請求ステータス情報の追加";
                 cntStatus = billingRepository.insertBillingStatus(targetMonth);
                 logger.info("{}件追加しました。", cntStatus);
 
-                logger.info("{}分の請求データ情報を追加しています。", targetMonth);
+                logger.info("{}分の請求データ情報を追加しています。", formattedTargetMonth);
                 phase = "請求データ情報の追加";
                 cntData = billingRepository.insertBillingData(targetMonth, lastDay);
                 logger.info("{}件追加しました。", cntData);
 
-                logger.info("{}分の請求明細データ情報を追加しています。", targetMonth);
+                logger.info("{}分の請求明細データ情報を追加しています。", formattedTargetMonth);
                 phase = "請求明細データ情報の追加";
                 cntDetailData = billingRepository.insertBillingDetailData(targetMonth, lastDay);
                 logger.info("{}件追加しました。", cntDetailData);
